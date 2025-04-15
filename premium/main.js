8f9477c71402542616b5dc2d471e6f401751d321
afif500 <afifabdullah0109@gmail.com>
2025-04-15 19:59:54 +0700

    first commit

---
+++ premium/main.js
@@ -0,0 +1,5 @@
+let trigger = document.getElementById("trigger")
+let notif = document.getElementById("notif")
+trigger.addEventListener("click", function() {
+    notif.style.transform = "translateX(0%)"
+})
